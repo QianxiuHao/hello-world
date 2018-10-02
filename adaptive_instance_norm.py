cfb411c6de52834540afc56c6f07a59309ba0b23
QianxiuHao <31768297+QianxiuHao@users.noreply.github.com>
2018-10-02 13:23:17 +0800

    Create adaptive_instance_norm.py

---
+++ adaptive_instance_norm.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  1 23:16:08 2018
+
+@author: Administrator
+"""
+# Adaptive Instance Normalization
+
+import tensorflow as tf
+
+def AdaIN(content, style, epsilon=1e-5):
+    meanC, varC = tf.nn.moments(content, [1, 2], keep_dims=True)
+    meanS, varS = tf.nn.moments(style,   [1, 2], keep_dims=True)
+
+    sigmaC = tf.sqrt(tf.add(varC, epsilon))
+    sigmaS = tf.sqrt(tf.add(varS, epsilon))
+    
+    return (content - meanC) * sigmaS / sigmaC + meanS
+
+
+
